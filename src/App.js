b739727d598b143adf6852f252cd2a647c3e7429
Siddharth Nair <sidsuresh@hotmail.com>
2023-08-02 17:49:38 +0400

    Added Portfolio and Home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Portfolio from "./Components/Home/Portfolio/Portfolio";
+import Profile from "./Components/Home/Profile/Profile";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Profile />
+      <Portfolio />
+    </div>
+  );
 }
 
 export default App;
